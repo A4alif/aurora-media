15fd00b1501ce467421a21793c7bea0c486a43b9
alifshah <alifhasanshah@gmail.com>
2023-01-09 12:38:41 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import { Box } from "@mui/system";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ChannelDetail from "./components/ChannelDetail";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import SearchFeed from "./components/SearchFeed";
+import VideoDetail from './components/VideoDetail';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Youtube clone boilerplate</h1>
-    </div>
+    <>
+      <Router>
+        <Box sx={{ backgroundColor: '#000'}}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element= {<Feed />} />
+            <Route path="/video/:id" element= {<VideoDetail />} />
+            <Route path="/channel/:id" element= {<ChannelDetail />} />
+            <Route path="/search/:searchTerm" element= {<SearchFeed />} />
+          </Routes>
+        </Box>
+      </Router>
+    </>
   );
 }
 
